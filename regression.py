7138e0e5aec3f6f6b836cf1488988539bb5234be
Mihir Kondapalli <mihirkondapalli@Mueller.physics.ucsb.edu>
2024-05-31 01:55:05 -0700

    added brute force regression

--- regression.py
+++ regression.py
@@ -1,7 +1,8 @@
-from functools import partial
 import jax
 import jax.numpy as jnp
+import numpy as np
 from disk_utils_jax import jax_model, jax_model_1d
+from functools import partial
 
 
 @partial(jax.jit, static_argnums=(0,1))
@@ -23,3 +24,46 @@ def likelihood_1d(disk_params, DistrModel, FuncModel, spf_params, flux_scaling, 
     result = jnp.power((target_image - model_image), 2) / sigma2 + jnp.log(sigma2)
     result = jnp.where(jnp.isnan(result), 0, result)
     return -0.5 * jnp.sum(result)
+
+# Brute Force Regression Method
+def manual_regression(target_image, DistrModel, FuncModel, spf_params, flux_scaling=1e6, bounds=None):
+    # 0: alpha_in, 1: alpha_out, 2: sma, 3: inclination, 4: position_angle
+    disk_params = np.array([3, -3, 20, 20, 5])
+
+    min_val = -likelihood_1d(disk_params, DistrModel, FuncModel, spf_params, flux_scaling, target_image)
+
+    min_ain, min_aout, min_sma, min_inc, min_pa = 3, -3, 20, 20, 5
+
+    for i in range(min_ain, 10):
+        disk_params[0] = float(i)
+
+        for j in range(min_aout, -10, -1):
+            disk_params[1] = float(j)
+
+            for k in range(min_sma, 80):
+                disk_params[2] = float(k)
+
+                for l in range(min_inc, 70):
+                    disk_params[3] = float(l)
+
+                    for m in range(min_pa, 20):
+                        disk_params[4] = float(m)
+
+                        val = -likelihood_1d(disk_params, DistrModel, FuncModel, spf_params, flux_scaling, target_image)
+
+                        if(val < min_val):
+                            min_ain = i
+                            min_aout = j
+                            min_sma = k
+                            min_inc = l
+                            min_pa = m
+                            min_val = val
+
+    disk_params[0] = min_ain
+    disk_params[1] = min_aout
+    disk_params[2] = min_sma
+    disk_params[3] = min_inc
+    disk_params[4] = min_pa
+
+    #return {'alpha_in': min_ain, 'alpha_out': min_aout, 'sma': min_sma, 'inclination': min_inc, 'position_angle': min_pa, 'flux_scaling': flux_scaling}
+    return {'alpha_in': min_ain, 'alpha_out': min_aout, 'sma': min_sma, 'inclination': min_inc, 'position_angle': min_pa, 'flux_scaling': flux_scaling}
