fd822fcf2c146f36307e29fce6018e9762c66a55
Mihir Kondapalli <mihirkondapalli@Mueller.physics.ucsb.edu>
2024-05-31 11:13:24 -0700

    some changes

--- regression.py
+++ regression.py
@@ -8,9 +8,8 @@ from functools import partial
 @partial(jax.jit, static_argnums=(0,1))
 def likelihood(DistrModel, FuncModel, disk_params, spf_params, target_image, err_map=jnp.ones([49, 140, 140])):
     model_image = jax_model(DistrModel, FuncModel, disk_params=disk_params, spf_params=spf_params) # (y)
-    log_target = jnp.log(target_image)
     yerr = jnp.power((model_image-target_image)/(err_map),2)
-    sigma2 = jnp.power(yerr, 2) + jnp.power(model_image, 2) * jnp.exp(2 * log_target)
+    sigma2 = jnp.power(yerr, 2) + jnp.power(model_image, 2)
     result = jnp.power((target_image - model_image), 2) / sigma2 + jnp.log(sigma2)
     result = jnp.where(jnp.isnan(result), 0, result)
     return -0.5 * jnp.sum(result)
@@ -18,9 +17,8 @@ def likelihood(DistrModel, FuncModel, disk_params, spf_params, target_image, err
 @partial(jax.jit, static_argnums=(1,2))
 def likelihood_1d(disk_params, DistrModel, FuncModel, spf_params, flux_scaling, target_image, err_map=jnp.ones([49, 140, 140])):
     model_image = jax_model_1d(DistrModel, FuncModel, disk_params, spf_params, flux_scaling) # (y)
-    log_target = jnp.log(target_image)
     yerr = jnp.power((model_image-target_image)/(err_map),2)
-    sigma2 = jnp.power(yerr, 2) + jnp.power(model_image, 2) * jnp.exp(2 * log_target)
+    sigma2 = jnp.power(yerr, 2) + jnp.power(model_image, 2)
     result = jnp.power((target_image - model_image), 2) / sigma2 + jnp.log(sigma2)
     result = jnp.where(jnp.isnan(result), 0, result)
     return -0.5 * jnp.sum(result)
@@ -32,7 +30,7 @@ def manual_regression(target_image, DistrModel, FuncModel, spf_params, flux_scal
 
     min_val = -likelihood_1d(disk_params, DistrModel, FuncModel, spf_params, flux_scaling, target_image)
 
-    min_ain, min_aout, min_sma, min_inc, min_pa = 3, -3, 20, 20, 5
+    min_ain, min_aout, min_sma, min_inc, min_pa = 3, -3, 20, 20, 10
 
     for i in range(min_ain, 10):
         disk_params[0] = float(i)
@@ -40,13 +38,13 @@ def manual_regression(target_image, DistrModel, FuncModel, spf_params, flux_scal
         for j in range(min_aout, -10, -1):
             disk_params[1] = float(j)
 
-            for k in range(min_sma, 80):
+            for k in range(min_sma, 60):
                 disk_params[2] = float(k)
 
-                for l in range(min_inc, 70):
+                for l in range(min_inc, 55):
                     disk_params[3] = float(l)
 
-                    for m in range(min_pa, 20):
+                    for m in range(min_pa, 50):
                         disk_params[4] = float(m)
 
                         val = -likelihood_1d(disk_params, DistrModel, FuncModel, spf_params, flux_scaling, target_image)
@@ -58,6 +56,8 @@ def manual_regression(target_image, DistrModel, FuncModel, spf_params, flux_scal
                             min_inc = l
                             min_pa = m
                             min_val = val
+
+        print(i)
 
     disk_params[0] = min_ain
     disk_params[1] = min_aout
