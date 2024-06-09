1db998ec4988a6e57e111f1138a2b3f88ddb26d9
Mihir Kondapalli <mihirkondapalli@Mueller.physics.ucsb.edu>
2024-06-08 23:44:04 -0700

    added jaxopt solver

--- regression.py
+++ regression.py
@@ -20,3 +20,11 @@ def log_likelihood_1d(disk_params, DistrModel, FuncModel, spf_params, flux_scali
     result = jnp.power((target_image - model_image), 2) / sigma2 + jnp.log(sigma2)
     result = jnp.where(jnp.isnan(result), 0, result)
     return -0.5 * jnp.sum(result)
+
+@partial(jax.jit, static_argnums=(1,2))
+def log_likelihood_1d_pos(disk_params, DistrModel, FuncModel, spf_params, flux_scaling, target_image, err_map):
+    model_image = jax_model_1d(DistrModel, FuncModel, disk_params, spf_params, flux_scaling) # (y)
+    sigma2 = jnp.power(err_map, 2) 
+    result = jnp.power((target_image - model_image), 2) / sigma2 + jnp.log(sigma2)
+    result = jnp.where(jnp.isnan(result), 0, result)
+    return 0.5 * jnp.sum(result)
