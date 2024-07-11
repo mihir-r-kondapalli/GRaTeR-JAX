cd78352533bc2988ae566f73f23aecedb6692223
mihir-r-kondapalli <mihir.kondapalli@gmail.com>
2024-07-11 12:13:44 -0700

    Analyzing Disk Fits plus some other stuff

--- disk_utils_jax.py
+++ disk_utils_jax.py
@@ -5,10 +5,10 @@ from SLD_utils import DustEllipticalDistribution2PowerLaws, DoubleHenyeyGreenste
 from functools import partial
 import jax.scipy.signal as jss
 
-@partial(jax.jit, static_argnums=(0,1))
-def jax_model(DistrModel, FuncModel, disk_params, spf_params,
-                  halfNbSlices=25, ksi0=3., gamma=2., beta=1., dstar=111.61,
-                  nx=140, ny=140, pixel_scale=0.063, n_nodes=6, pxInArcsec=0.01225, distance=50.):
+@partial(jax.jit, static_argnums=(0,1,4))
+def jax_model(DistrModel, FuncModel, disk_params, spf_params, PSFModel = None,
+                  halfNbSlices=25, ksi0=3., gamma=2., beta=1.,
+                  nx=140, ny=140, pxInArcsec=0.01414, distance=50.):
 
     distr_params = DistrModel.init(accuracy=5.e-3, ain=disk_params['alpha_in'], aout=disk_params['alpha_out'], a=disk_params['sma'],
                                    e=0., ksi0=ksi0, gamma=gamma, beta=beta, amin=0., dens_at_r0=1.)
@@ -35,14 +35,19 @@ def jax_model(DistrModel, FuncModel, disk_params, spf_params,
     scattered_light_image = ScatteredLightDisk.compute_scattered_light_jax(disk_params_jax, distr_params, DistrModel, func_params, FuncModel,
                                                                   x_vector, y_vector, scattered_light_map, image, limage, tmp,
                                                                   halfNbSlices)
+
+    if(PSFModel!=None):
+        psf_image = PSFModel.generate(jnp.meshgrid(x_vector, y_vector))
+        disk_image_psf = jss.convolve2d(scattered_light_image, psf_image)[70:210:, 70:210:]
     
     return disk_params['flux_scaling']*scattered_light_image
 
 
 # 0: alpha_in, 1: alpha_out, 2: sma, 3: inclination, 4: position_angle
-@partial(jax.jit, static_argnums=(0,1))
-def jax_model_1d(DistrModel, FuncModel, disk_params, spf_params, flux_scaling, halfNbSlices=25, ksi0=3., gamma=2., beta=1.,
-                 dstar=111.61, nx=140, ny=140, pixel_scale=0.063, n_nodes=6, pxInArcsec=0.01225, distance=50., inc = 0):
+@partial(jax.jit, static_argnums=(0,1,5))
+def jax_model_1d(DistrModel, FuncModel, disk_params, spf_params, flux_scaling, PSFModel = None,
+                halfNbSlices=25, ksi0=3., gamma=2., beta=1.,
+                nx=140, ny=140, pxInArcsec=0.01414, distance=50.):
 
     distr_params = DistrModel.init(accuracy=5.e-3, ain=disk_params[0], aout=disk_params[1], a=disk_params[2],
                                    e=0., ksi0=ksi0, gamma=gamma, beta=beta, amin=0., dens_at_r0=1.)
@@ -70,13 +75,18 @@ def jax_model_1d(DistrModel, FuncModel, disk_params, spf_params, flux_scaling, h
                                                                   x_vector, y_vector, scattered_light_map, image, limage, tmp,
                                                                   halfNbSlices)
     
+    if(PSFModel!=None):
+        psf_image = PSFModel.generate(jnp.meshgrid(x_vector, y_vector))
+        scattered_light_image = jss.convolve2d(scattered_light_image, psf_image)[70:210:, 70:210:]
+
     return flux_scaling*scattered_light_image
 
 
 # 0: alpha_in, 1: alpha_out, 2: sma, 3: inclination, 4: position_angle
-@partial(jax.jit, static_argnums=(0,1))
-def jax_model_all_1d(DistrModel, FuncModel, disk_params, spf_params, flux_scaling, halfNbSlices=25, ksi0=3., gamma=2., beta=1.,
-                 dstar=111.61, nx=140, ny=140, pixel_scale=0.063, n_nodes=6, pxInArcsec=0.01225, distance=50.):
+@partial(jax.jit, static_argnums=(0,1,5))
+def jax_model_all_1d(DistrModel, FuncModel, disk_params, spf_params, flux_scaling,
+                     PSFModel = None, halfNbSlices=25, ksi0=3., gamma=2., beta=1.,
+                    nx=140, ny=140, pxInArcsec=0.01414, distance=50.):
 
     distr_params = DistrModel.init(accuracy=5.e-3, ain=disk_params[0], aout=disk_params[1], a=disk_params[2],
                                    e=0., ksi0=ksi0, gamma=gamma, beta=beta, amin=0., dens_at_r0=1.)
@@ -101,14 +111,18 @@ def jax_model_all_1d(DistrModel, FuncModel, disk_params, spf_params, flux_scalin
     scattered_light_image = ScatteredLightDisk.compute_scattered_light_jax(disk_params_jax, distr_params, DistrModel, spf_params,
                                                                 FuncModel, x_vector, y_vector, scattered_light_map, image, limage,
                                                                 tmp, halfNbSlices)
+
+    if(PSFModel!=None):
+        psf_image = PSFModel.generate(jnp.meshgrid(x_vector, y_vector))
+        scattered_light_image = jss.convolve2d(scattered_light_image, psf_image)[70:210:, 70:210:]
     
     return flux_scaling*scattered_light_image
 
 
-@partial(jax.jit, static_argnums=(0,1))
-def jax_model_spline(DistrModel, FuncModel, disk_params, spf_params,
-                  halfNbSlices=25, ksi0=3., gamma=2., beta=1., dstar=111.61,
-                  nx=140, ny=140, pixel_scale=0.063, n_nodes=6, pxInArcsec=0.01225, distance=50., knots=jnp.linspace(1,-1,6)):
+@partial(jax.jit, static_argnums=(0,1,4))
+def jax_model_spline(DistrModel, FuncModel, disk_params, spf_params, PSFModel = None,
+                  halfNbSlices=25, ksi0=3., gamma=2., beta=1.,
+                  nx=140, ny=140, pxInArcsec=0.01414, distance=50., knots=jnp.linspace(1,-1,6)):
 
     distr_params = DistrModel.init(accuracy=5.e-3, ain=disk_params['alpha_in'], aout=disk_params['alpha_out'], a=disk_params['sma'],
                                    e=0., ksi0=ksi0, gamma=gamma, beta=beta, amin=0., dens_at_r0=1.)
@@ -135,25 +149,27 @@ def jax_model_spline(DistrModel, FuncModel, disk_params, spf_params,
     scattered_light_image = ScatteredLightDisk.compute_scattered_light_jax(disk_params_jax, distr_params, DistrModel, func_params, FuncModel,
                                                                   x_vector, y_vector, scattered_light_map, image, limage, tmp,
                                                                   halfNbSlices)
+
+    if(PSFModel!=None):
+        psf_image = PSFModel.generate(jnp.meshgrid(x_vector, y_vector))
+        scattered_light_image = jss.convolve2d(scattered_light_image, psf_image)[70:210:, 70:210:]
     
     return disk_params['flux_scaling']*scattered_light_image
 
 
+# 0: alpha_in, 1: alpha_out, 2: sma, 3: inclination, 4: position_angle
 @partial(jax.jit, static_argnums=(0,1,5))
-def jax_model_psf(DistrModel, FuncModel, disk_params, spf_params, psf_params, PSFModel = GAUSSIAN_PSF,
-                  halfNbSlices=25, ksi0=3., gamma=2., beta=1., dstar=111.61,
-                  nx=140, ny=140, pixel_scale=0.063, n_nodes=6, pxInArcsec=0.01225, distance=50.):
+def jax_model_all_1d_cent(DistrModel, FuncModel, disk_params, spf_params, flux_scaling,
+                     PSFModel = None, halfNbSlices=25, ksi0=3., gamma=2., beta=1.,
+                     nx=140, ny=140, xc=70, yc=70,
+                    pxInArcsec=0.01414, distance=50.):
 
-    distr_params = DistrModel.init(accuracy=5.e-3, ain=disk_params['alpha_in'], aout=disk_params['alpha_out'], a=disk_params['sma'],
+    distr_params = DistrModel.init(accuracy=5.e-3, ain=disk_params[0], aout=disk_params[1], a=disk_params[2],
                                    e=0., ksi0=ksi0, gamma=gamma, beta=beta, amin=0., dens_at_r0=1.)
-    disk_params_jax = ScatteredLightDisk.init(distr_params, disk_params['inclination'], disk_params['position_angle'],
-                                              disk_params['alpha_in'], disk_params['alpha_out'], disk_params['sma'],
+    disk_params_jax = ScatteredLightDisk.init(distr_params, disk_params[3], disk_params[4],
+                                              disk_params[0], disk_params[1], disk_params[2],
                                               nx=nx, ny=ny, distance = distance, omega =0., pxInArcsec=pxInArcsec,
                                               xdo=0., ydo=0.)
-
-    yc, xc = ny, nx
-    xc = jnp.where(nx%2==1, nx/2-0.5, nx/2).astype(int)
-    yc = jnp.where(ny%2==1, ny/2-0.5, ny/2).astype(int)
 
     x_vector = (jnp.arange(0, nx) - xc)*pxInArcsec*distance
     y_vector = (jnp.arange(0, ny) - yc)*pxInArcsec*distance
@@ -164,13 +180,12 @@ def jax_model_psf(DistrModel, FuncModel, disk_params, spf_params, psf_params, PS
     limage = jnp.zeros([2*halfNbSlices-1, ny, nx])
     tmp = jnp.arange(0, halfNbSlices)
     
-    func_params = FuncModel.pack_pars(spf_params)
-    
-    scattered_light_image = ScatteredLightDisk.compute_scattered_light_jax(disk_params_jax, distr_params, DistrModel, func_params, FuncModel,
-                                                                  x_vector, y_vector, scattered_light_map, image, limage, tmp,
-                                                                  halfNbSlices)
-    
-    psf_image = PSFModel.generate(jnp.meshgrid(x_vector, y_vector), PSFModel.init(psf_params))
-    disk_image_psf = jss.convolve2d(scattered_light_image, psf_image)
+    scattered_light_image = ScatteredLightDisk.compute_scattered_light_jax(disk_params_jax, distr_params, DistrModel, spf_params,
+                                                                FuncModel, x_vector, y_vector, scattered_light_map, image, limage,
+                                                                tmp, halfNbSlices)
 
-    return disk_params['flux_scaling']*disk_image_psf
+    if(PSFModel!=None):
+        psf_image = PSFModel.generate(jnp.meshgrid(x_vector, y_vector))
+        scattered_light_image = jss.convolve2d(scattered_light_image, psf_image)[70:210:, 70:210:]
+    
+    return flux_scaling*scattered_light_image
---
+++ optimize.py
@@ -0,0 +1,52 @@
+from regression import log_likelihood_1d_pos_all_pars_spline, log_likelihood_1d_full_opt
+from disk_utils_jax import jax_model_all_1d, jax_model_all_1d_cent
+from scipy.optimize import minimize
+from SLD_utils import *
+import jax.numpy as jnp
+
+
+FULL_BOUNDS = [(1, 10), (-1, 10), (10, 80), (0, 180), (0, 180), (60, 80), (60, 80), (0, 10), (0, 10), (0, 10),
+              (0, 0.5), (0, 0.5), (0, 0.5), (0, 0.5), (0, 0.5), (0, 0.5)]
+
+def quick_optimize(target_image, err_map, flux_scaling=1e6, knots=jnp.linspace(1, -1, 6), disp = True, method = None,
+                    iters = 500, **kwargs):
+    init_knot_guess = DoubleHenyeyGreenstein_SPF.compute_phase_function_from_cosphi([0.5, 0.5, 0.5], knots)
+    init_disk_guess = jnp.array([5., -5., 45., 45, 45])
+    init_guess = jnp.concatenate([init_disk_guess, init_knot_guess])
+    llp = lambda x: log_likelihood_1d_pos_all_pars_spline(x, DustEllipticalDistribution2PowerLaws, InterpolatedUnivariateSpline_SPF, 
+                        flux_scaling, target_image, err_map, **kwargs)
+    print(llp(init_guess).shape)
+    opt = {'disp':False,'maxiter':iters}
+    soln = minimize(llp, init_guess, options=opt, method=method)
+    if(disp):
+        print(soln)
+    return soln.x
+
+def quick_image(pars, flux_scaling=1e6, **kwargs):
+    return jax_model_all_1d(DustEllipticalDistribution2PowerLaws, InterpolatedUnivariateSpline_SPF, pars[0:5],
+                                InterpolatedUnivariateSpline_SPF.pack_pars(pars[5:]), flux_scaling, **kwargs)
+
+# 0: alpha_in, 1: alpha_out, 2: sma, 3: inclination, 4: position_angle, 5: xc, 6: yc, 7: ksi, 8: gamma, 9: beta
+# 10 onwards is spline parameters, pxInArcsec and distance are good kwargs to include
+def quick_optimize_full_opt(target_image, err_map, flux_scaling=1e6, knots=jnp.linspace(1, -1, 6), disp = True, method = None,
+                    iters = 500, bounds = None, **kwargs):
+
+    init_knot_guess = DoubleHenyeyGreenstein_SPF.compute_phase_function_from_cosphi([0.5, 0.5, 0.5], knots)
+    init_disk_guess = jnp.array([5., -5., 45., 45, 45])
+    init_cent_guess = jnp.array([70., 70.])
+    ksi_gamma_beta_guess = jnp.array([3, 2, 1])
+    init_guess = jnp.concatenate([init_disk_guess, init_cent_guess, ksi_gamma_beta_guess, init_knot_guess])
+    llp = lambda x: log_likelihood_1d_full_opt(x, 
+                        DustEllipticalDistribution2PowerLaws, InterpolatedUnivariateSpline_SPF, 
+                        flux_scaling, target_image, err_map, **kwargs)
+    print(llp(init_guess).shape)
+    opt = {'disp':False,'maxiter':iters}
+    soln = minimize(llp, init_guess, options=opt, method=method, bounds=bounds)
+    if(disp):
+        print(soln)
+    return soln.x
+
+def quick_image_full_opt(pars, flux_scaling=1e6, **kwargs):
+    return jax_model_all_1d_cent(DustEllipticalDistribution2PowerLaws, InterpolatedUnivariateSpline_SPF, pars[0:5],
+                                InterpolatedUnivariateSpline_SPF.pack_pars(pars[10:]), flux_scaling,
+                                xc=pars[5], yc=pars[6], ksi0=pars[7], gamma=pars[8], beta=pars[9],**kwargs)
