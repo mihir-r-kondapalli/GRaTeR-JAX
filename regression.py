5193e409b91fcb69b9b93b0920a07760e141746c
mihir-r-kondapalli <mihir.kondapalli@gmail.com>
2024-07-25 19:21:17 -0700

    some cleanup, changes + mcmc testing

--- regression.py
+++ regression.py
@@ -11,7 +11,7 @@ def log_likelihood_image(model_image, target_image, err_map):
     sigma2 = jnp.power(err_map, 2)
     result = jnp.power((target_image - model_image), 2) / sigma2 + jnp.log(sigma2)
     result = jnp.where(jnp.isnan(result), 0, result)
-    return -0.5 * jnp.sum(result)
+    return -0.5 * jnp.sum(result) / jnp.size(target_image)
 
 # Computes the error between the target_image and a disk generated from the parameters
 @partial(jax.jit, static_argnums=(0,1,6))
@@ -21,7 +21,7 @@ def log_likelihood(DistrModel, FuncModel, disk_params, spf_params, target_image,
     sigma2 = jnp.power(err_map, 2)
     result = jnp.power((target_image - model_image), 2) / sigma2 + jnp.log(sigma2)
     result = jnp.where(jnp.isnan(result), 0, result)
-    return -0.5 * jnp.sum(result)
+    return -0.5 * jnp.sum(result) / jnp.size(target_image)
 
 # Computes the error between the target_image and a disk generated from the parameters (disk_params is a jax array)
 @partial(jax.jit, static_argnums=(1,2,7))
@@ -31,7 +31,7 @@ def log_likelihood_1d(disk_params, DistrModel, FuncModel, spf_params, flux_scali
     sigma2 = jnp.power(err_map, 2) 
     result = jnp.power((target_image - model_image), 2) / sigma2 + jnp.log(sigma2)
     result = jnp.where(jnp.isnan(result), 0, result)
-    return -0.5 * jnp.sum(result)
+    return -0.5 * jnp.sum(result) / jnp.size(target_image)
 
 # Computes the error between the target_image and a disk generated from the parameters (disk_params is a jax array)
 # Returns a positive number instead of negative number for future use
@@ -42,7 +42,7 @@ def log_likelihood_1d_pos(disk_params, DistrModel, FuncModel, spf_params, flux_s
     sigma2 = jnp.power(err_map, 2) 
     result = jnp.power((target_image - model_image), 2) / sigma2 + jnp.log(sigma2)
     result = jnp.where(jnp.isnan(result), 0, result)
-    return 0.5 * jnp.sum(result)
+    return 0.5 * jnp.sum(result) / jnp.size(target_image)
 
 # Computes the error between the target_image and a disk generated from the parameters (disk_and_spf_params is a jax array
 # and a combination of a disk_params array and an spf_param array) (first 6 values are disk's, rest are spf's)
@@ -55,7 +55,7 @@ def log_likelihood_1d_pos_all_pars(disk_and_spf_params, DistrModel, FuncModel, f
     sigma2 = jnp.power(err_map, 2) 
     result = jnp.power((target_image - model_image), 2) / sigma2 + jnp.log(sigma2)
     result = jnp.where(jnp.isnan(result), 0, result)
-    return 0.5 * jnp.sum(result)
+    return 0.5 * jnp.sum(result) / jnp.size(target_image)
 
 # Computes the error between the target_image and a disk generated from the parameters (disk_and_spf_params is a jax array
 # and a combination of a disk_params array and an spf_param array) (first 6 values are disk's, rest are spf's)
@@ -69,7 +69,7 @@ def log_likelihood_1d_pos_all_pars_spline(disk_and_spf_params, DistrModel, FuncM
     sigma2 = jnp.power(err_map, 2)
     result = jnp.power((target_image - model_image), 2) / sigma2 + jnp.log(sigma2)
     result = jnp.where(jnp.isnan(result), 0, result)
-    return 0.5 * jnp.sum(result)
+    return 0.5 * jnp.sum(result) / jnp.size(target_image)
 
 # Computes the error between the target_image and a disk generated from the parameters (both disk_params
 # and spf_params are jax arrays)
@@ -83,7 +83,7 @@ def log_likelihood_1d_pos_spline(disk_params, spf_params, DistrModel, FuncModel,
     sigma2 = jnp.power(err_map, 2)
     result = jnp.power((target_image - model_image), 2) / sigma2 + jnp.log(sigma2)
     result = jnp.where(jnp.isnan(result), 0, result)
-    return 0.5 * jnp.sum(result)
+    return 0.5 * jnp.sum(result) / jnp.size(target_image)
 
 
 # Basically log_likelihood_1d_pos_all_pars_spline but including cent as the first two values in the parameter array
@@ -95,7 +95,7 @@ def log_likelihood_1d_pos_cent(params, DistrModel, FuncModel, flux_scaling, targ
     sigma2 = jnp.power(err_map, 2)
     result = jnp.power((target_image - model_image), 2) / sigma2 + jnp.log(sigma2)
     result = jnp.where(jnp.isnan(result), 0, result)
-    return 0.5 * jnp.sum(result)
+    return 0.5 * jnp.sum(result) / jnp.size(target_image)
 
 
 # Computes the error between the target_image and a disk generated from the parameters (all_params is a jax array)
@@ -112,4 +112,4 @@ def log_likelihood_1d_full_opt(all_params, DistrModel, FuncModel, flux_scaling, 
     sigma2 = jnp.power(err_map, 2)
     result = jnp.where(err_map > 0, jnp.power((target_image - model_image), 2) / (sigma2+1e-8) + jnp.log(sigma2+1e-8), 0)
     result = jnp.where(jnp.isnan(result), 0, result)
-    return 0.5 * jnp.sum(result)
+    return 0.5 * jnp.sum(result) / jnp.size(target_image)
