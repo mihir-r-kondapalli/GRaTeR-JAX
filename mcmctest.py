a96b9a6240803f9f237c1574a42341715a9bde88
mihir-r-kondapalli <mihir.kondapalli@gmail.com>
2024-08-05 14:46:40 -0700

    mcmc stuff

--- mcmctest.py
+++ mcmctest.py
@@ -15,116 +15,13 @@ from datetime import datetime
 from mcmc_model import MCMC_model
 from tqdm import tqdm
 
+from mctest_utils import (process_image, create_empirical_err_map, get_inc_bounded_knots, get_aic, get_bic,
+                          transpose_even_to_odd_spline, plot_mc_img)
+
 import os
 os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '0.3'
 
 jax.config.update("jax_enable_x64", True)
-
-###############################################################################
-# Helper Methods
-
-def create_empirical_err_map(data, annulus_width=5, mask_rad=9, outlier_pixels=None):    
-    y,x = np.indices(data.shape)
-    y -= data.shape[0]//2
-    x -= data.shape[1]//2 
-    radii = np.sqrt(x**2 + y**2) 
-    noise_array = np.zeros_like(data)
-    for i in range(0, int(np.max(radii)//annulus_width) ): 
-        indices = (radii > i*annulus_width) & (radii <= (i+1)*annulus_width) 
-        noise_array[indices] = np.nanstd(data[indices])
-    mask = radii <= mask_rad
-    noise_array[mask] = 0
-
-    if(outlier_pixels != None):
-        for pixel in outlier_pixels:
-            noise_array[pixel[0]][pixel[1]] = noise_array[pixel[0]][pixel[1]] * 1e6 
-
-    return noise_array
-
-def process_image(image, scale_factor=1, offset=1):
-    scaled_image = (image[::scale_factor, ::scale_factor])[1::, 1::]
-    cropped_image = image[70:210, 70:210]
-    def safe_float32_conversion(value):
-        try:
-            return np.float32(value)
-        except (ValueError, TypeError):
-            print("This value is unjaxable: " + str(value))
-    fin_image = np.nan_to_num(cropped_image)
-    fin_image = np.vectorize(safe_float32_conversion)(fin_image)
-    return fin_image
-
-def get_inc_bounded_knots(inclination, radius, buffer = 0, num_knots=-1):
-    if(num_knots <= 0):
-        if(radius < 50):
-            num_knots = 4
-        else:
-            num_knots = 6
-    return jnp.linspace(jnp.cos(jnp.deg2rad(90-inclination-buffer)), jnp.cos(jnp.deg2rad(90+inclination+buffer)), num_knots)
-
-def transpose_even_to_odd_spline(knot_vals, knots, new_knots):
-    x_vals = jnp.linspace(-1, 1, 100)
-    spline = InterpolatedUnivariateSpline_SPF.init(knot_vals, knots=knots)
-    y_vals = spline(x_vals)
-
-    new_k_vals = []
-    for nk in new_knots:
-        new_k_vals.append(y_vals[int((nk*100 + 100)/2)])
-    return jnp.array(new_k_vals)
-
-def plot_mc_img(target_image, sc_image, mc_image, init_val, fin_val):
-
-    ## Get a good scaling
-    y, x = np.indices(target_image.shape)
-    y -= 70
-    x -= 70 
-    rads = np.sqrt(x**2+y**2)
-    mask = (rads > 12)
-
-    vmin = np.nanpercentile(target_image[mask], 1)
-    vmax = np.nanpercentile(target_image[mask], 99.9)
-
-    fig, axes = plt.subplots(3,3, figsize=(20,10))
-
-    im = axes[0][0].imshow(target_image, origin='lower', cmap='inferno')
-    axes[0][0].set_title("Target Image")
-    plt.colorbar(im, ax=axes[0][0], shrink=0.75)
-    im.set_clim(vmin, vmax)
-
-    im = axes[0][1].imshow(sc_image, origin='lower', cmap='inferno')
-    axes[0][1].set_title("Scipy Fitted Image: " + str(init_val))
-    plt.colorbar(im, ax=axes[0][1], shrink=0.75)
-    im.set_clim(vmin, vmax)
-
-    im = axes[1][0].imshow(mc_image, origin='lower', cmap='inferno')
-    axes[1][0].set_title("MCMC Fitted Image: " + str(fin_val))
-    plt.colorbar(im, ax=axes[0][2], shrink=0.75)
-    im.set_clim(vmin, vmax)
-
-    im = axes[1][1].imshow(target_image-sc_image, origin='lower', cmap='inferno')
-    axes[1][1].set_title("Scipy Residual")
-    plt.colorbar(im, ax=axes[1][1], shrink=0.75)
-    im.set_clim(vmin, vmax)
-
-    im = axes[1][2].imshow(target_image-mc_image, origin='lower', cmap='inferno')
-    axes[1][2].set_title("MCMC Residual")
-    plt.colorbar(im, ax=axes[1][2], shrink=0.75)
-    im.set_clim(vmin, vmax)
-
-    im = axes[2][1].imshow((target_image-sc_image)/err_map, origin='lower', cmap='inferno')
-    axes[2][1].set_title("Scipy Residual / Noise")
-    plt.colorbar(im, ax=axes[2][1], shrink=0.75)
-    im.set_clim(vmin, vmax)
-
-    im = axes[2][2].imshow((target_image-mc_image)/err_map, origin='lower', cmap='inferno')
-    axes[2][2].set_title("MCMC Residual / Noise")
-    plt.colorbar(im, ax=axes[2][2], shrink=0.75)
-    im.set_clim(vmin, vmax)
-
-def get_aic(pos_log_likelihood, num_params):
-    return 2 * pos_log_likelihood + 2 * num_params
-
-def get_bic(pos_log_likelihood, num_params):
-    return 2 * pos_log_likelihood + np.log(140*140) * num_params
 
 ###############################################################################
 
@@ -138,7 +35,7 @@ def fit_spline(row, target_image, err_map, disp = False, flux_scaling=1e6):
 
     bounds = []
     for i in range(0, int(row["Knots"])):
-        bounds.append((0, 0.1))
+        bounds.append((1e-8, 0.1))
 
     llp_spline = lambda x: log_likelihood_1d_pos_cent(jnp.concatenate([disk_pars, x]), 
                         DustEllipticalDistribution2PowerLaws, InterpolatedUnivariateSpline_SPF,
@@ -162,7 +59,7 @@ def run_mcmc_ab(soln, target_image, err_map, row, name, nwalkers=250, niter=250,
 
     knots = get_inc_bounded_knots(row["Inclination"], row["Radius"], buffer = 0, num_knots=jnp.size(soln[7:]))
 
-    llp = lambda x: -log_likelihood_1d_pos_cent(x, 
+    llp = lambda x: -log_likelihood_1d_pos_cent(jnp.concatenate([x[0:7], jnp.exp(x[7:])]), 
                         DustEllipticalDistribution2PowerLaws, InterpolatedUnivariateSpline_SPF, 
                         1e6, target_image, err_map, PSFModel = EMP_PSF, pxInArcsec=0.01414,
                         distance = row["Distance"], knots=knots)
@@ -170,20 +67,23 @@ def run_mcmc_ab(soln, target_image, err_map, row, name, nwalkers=250, niter=250,
 
     DISK_BOUNDS = np.array([np.array([0.1, -15, 0, 0, 0]), np.array([15, -0.1, 150, 180, 400])])
     CENT_BOUNDS = np.array([np.array([65, 65]), np.array([75, 75])])
-    SPLINE_BOUNDS = np.array([np.zeros(jnp.size(knots)), 0.1*np.ones(jnp.size(knots))])
-    BOUNDS = np.array([np.concatenate([CENT_BOUNDS[0], DISK_BOUNDS[0], SPLINE_BOUNDS[0]]),
-                        np.concatenate([CENT_BOUNDS[1], DISK_BOUNDS[1], SPLINE_BOUNDS[1]])])
+    SPLINE_BOUNDS = np.array([1e-8 * np.ones(jnp.size(knots)), 0.1 * np.ones(jnp.size(knots))])
+    BOUNDS = np.array([np.concatenate([CENT_BOUNDS[0], DISK_BOUNDS[0], np.log(SPLINE_BOUNDS[0])]),
+                        np.concatenate([CENT_BOUNDS[1], DISK_BOUNDS[1], np.log(SPLINE_BOUNDS[1])])])
 
-    if not(np.all(soln > BOUNDS[0]) and np.all(soln < BOUNDS[1])):
+    if np.all(soln < BOUNDS[0]) and np.all(soln > BOUNDS[1]):
         print()
         print("Intial params out of bounds for MCMC!!!")
         print(soln)
         print(BOUNDS)
-        print(not(soln > BOUNDS[0] and soln < BOUNDS[1]))
+        oob = (soln < BOUNDS[0]) and (soln > BOUNDS[1])
+        print(oob)
         exit()
 
+    init_soln = jnp.concatenate([soln[0:7], jnp.log(soln[7:])])
+
     mc_model = MCMC_model(llp, BOUNDS)
-    mc_model.run(soln, nconst = 1e-7, nwalkers=nwalkers, niter=niter, burn_iter=burns)
+    mc_model.run(init_soln, nconst = 1e-7, nwalkers=nwalkers, niter=niter, burn_iter=burns)
 
     # Plotting Corner Plots
     labels = ['xc', 'yc', 'alpha_in', 'alpha_out', 'sma', 'inclination', 'position_angle']
@@ -207,8 +107,11 @@ def run_mcmc_ab(soln, target_image, err_map, row, name, nwalkers=250, niter=250,
     # Plotting Images
     sc_image = quick_image_cent(soln, PSFModel = EMP_PSF, pxInArcsec=0.01414, distance = row["Distance"], knots=knots)
     mc_soln = np.median(mc_model.sampler.flatchain, axis=0)
-    mc_image = quick_image_cent(mc_soln, PSFModel = EMP_PSF, pxInArcsec=0.01414, distance = row["Distance"], knots=knots)
-    plot_mc_img(target_image, sc_image, mc_image, round(-llp(soln), 3), round(-llp(mc_soln), 3))
+
+    # Need to adjust for log
+    mc_image = quick_image_cent(jnp.concatenate([mc_soln[0:7], jnp.exp(mc_soln[7:])]), PSFModel = EMP_PSF, pxInArcsec=0.01414, distance = row["Distance"], knots=knots)
+
+    plot_mc_img(name, err_map, target_image, sc_image, mc_image, round(-llp(init_soln), 3), round(-llp(mc_soln), 3))
     plt.savefig("mcmc_results/"+name+"_mcresid.png")
 
     return mc_soln, get_aic(-llp(mc_soln), len(mc_soln)), get_bic(-llp(mc_soln), len(mc_soln))
@@ -226,7 +129,7 @@ warnings.filterwarnings("ignore")
 
 num_disks = len(image_data)
 
-start = 7
+start = 0
 #num_disks = start+1
 
 for i in tqdm(range(start, num_disks)):
@@ -237,7 +140,7 @@ for i in tqdm(range(start, num_disks)):
     target_image = process_image(hdul['SCI'].data[1,:,:])
     err_map = process_image(create_empirical_err_map(hdul['SCI'].data[2,:,:])).astype(jnp.float64)
     soln = fit_spline(row, target_image, err_map)
-    mc_soln, aic, bic = run_mcmc_ab(soln, target_image, err_map, row, name)
+    mc_soln, aic, bic = run_mcmc_ab(soln, target_image, err_map, row, name, nwalkers = 300, niter = 300, burns = 60)
 
     # Print Messages
     print(str(i+1) + " of " + str(num_disks) + " done.")
