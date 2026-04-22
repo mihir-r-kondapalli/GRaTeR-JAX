c80846a1782f0c6543f983b748f0a890f03bd42a
mihir-r-kondapalli <mihir.kondapalli@gmail.com>
2026-04-22 09:23:21 -0700

    added kwarg for sum vs mean

--- grater_jax/disk_model/jax_gradient_wrappers.py
+++ grater_jax/disk_model/jax_gradient_wrappers.py
@@ -281,11 +281,7 @@ def jax_model_winnie_ll(DiskModel, DistrModel, FuncModel, winnie_psf, StellarPSF
 
 def jax_model_spline_ll(DiskModel, DistrModel, FuncModel, PSFModel, StellarPSFModel, disk_params, spf_params, psf_params, stellar_psf_params, target_image, err_map,
                      throughput, flux_scaling = 1e6, distance = 0., pxInArcsec = 0., nx = 140, ny = 140, halfNbSlices = 25,
-<<<<<<< HEAD
-                     knots=np.linspace(1,-1,7)):
-=======
                      knots=jnp.linspace(1,-1,7), ll_method = 'sum'):
->>>>>>> 89a7149 (feat(likelihood): add ll_method kwarg for sum/mean reduction)
 
     """
     Get the log likelihood for the generated disk model image given disk, scattering function, point spread function,
@@ -387,11 +383,7 @@ def jax_model_spline_ll(DiskModel, DistrModel, FuncModel, PSFModel, StellarPSFMo
 
 def jax_model_spline_winnie_ll(DiskModel, DistrModel, FuncModel, winnie_psf, StellarPSFModel, disk_params, spf_params, stellar_psf_params, target_image, err_map,
                      throughput, flux_scaling = 1e6, distance = 0., pxInArcsec = 0., nx = 140, ny = 140, halfNbSlices = 25,
-<<<<<<< HEAD
-                     knots=np.linspace(1,-1,7)):
-=======
                      knots=jnp.linspace(1,-1,7), ll_method = 'sum'):
->>>>>>> 89a7149 (feat(likelihood): add ll_method kwarg for sum/mean reduction)
 
     """
     Get the log likelihood for the generated disk model image given disk, scattering function, point spread function,
